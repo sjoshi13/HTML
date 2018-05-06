bf80f1650cf7cc365653cd4c2d0e4ba2963eb975
shruti joshi <36851727+sjoshi13@users.noreply.github.com>
2018-05-06 21:17:28 +0530

    Update multiply.cpp
    STEP3:
    Added multiply function which will be called from js

--- multiply.cpp
+++ multiply.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
+#include <emscripten.h>
 	using namespace std;
-
+extern "C"{
+	 EMSCRIPTEN_KEEPALIVE
+	int multiply(float x,float y)
+	{
+		return x*y;
+	
+	}
+}
 	int main()
 	{
 		cout << "initialized";
